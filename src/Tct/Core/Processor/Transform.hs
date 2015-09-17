f2077feb5d3689751ea6350a3ced57ebfe36511b
mschaper <michael.schaper@uibk.ac.at>
2015-09-17 14:11:53 +0200

    better Trasform processor
      - has description field for proof output
      - uses `(i -> Either String o)` for possible failing transormations,
    
    not backwards compatible

--- src/Tct/Core/Processor/Transform.hs
+++ src/Tct/Core/Processor/Transform.hs
@@ -1,20 +1,27 @@
 -- | This module provides a simple /Transform/ processor with a minimal proof output.
-module Tct.Core.Processor.Transform (transform) where
+module Tct.Core.Processor.Transform
+  ( transform
+  , transform')
+  where
 
 
-import qualified Tct.Core.Common.Pretty          as PP
-import qualified Tct.Core.Common.Xml             as Xml
+import qualified Tct.Core.Common.Pretty as PP
+import qualified Tct.Core.Common.Xml    as Xml
 import           Tct.Core.Data
 
 
-data Transform i o = Transform (i -> Return o)
+-- | @Transform descibe transform@
+--
+-- @describe@  A short description that is used in the proof output.
+-- @trasnform@ A failing transformation.
+data Transform i o = Transform String (i -> Either String o)
 
 instance Show (Transform i o) where
   show _ = "Problem Transformation."
 
 data TransformProof i o
-  = TransformProof i o
-  | TransformFail
+  = TransformProof String i o
+  | TransformFail String
   deriving Show
 
 instance (ProofData i, ProofData o) => Processor (Transform i o) where
@@ -22,29 +29,35 @@ instance (ProofData i, ProofData o) => Processor (Transform i o) where
   type I (Transform i o)           = i
   type O (Transform i o)           = o
 
-  solve p@(Transform t) prob = t prob `seq` return . resultToTree' p prob $ case t prob of
-    Halt _         -> Fail TransformFail
-    Abort _        -> Fail TransformFail
-    Continue nprob -> Success (toId nprob) (TransformProof prob nprob) fromId
+  solve p@(Transform msg t) prob = t prob `seq` return . resultToTree' p prob $ case t prob of
+    Left  err   -> Fail $ TransformFail err
+    Right nprob -> Success (toId nprob) (TransformProof msg prob nprob) fromId
 
 
--- | A Wrapper for a transfromation. With minimal proof output.
-transform :: (ProofData i, ProofData o) => (i -> Return o) -> Strategy i o
-transform = Proc . Transform
+-- | The /Transform/ strategy.
+transform :: (ProofData i, ProofData o)
+  => String                 -- ^ Description of the transformation; used in the proof output.
+  -> (i -> Either String o) -- ^ A (possible) failing transformation.
+  -> Strategy i o
+transform msg = Proc . Transform msg
 
+-- | >transform' = transform ""
+transform' :: (ProofData i, ProofData o) => (i -> Either String o) -> Strategy i o
+transform' = transform ""
 
 instance (PP.Pretty i, PP.Pretty o) => PP.Pretty (TransformProof i o) where
-  pretty TransformFail        = PP.text "The transformation failed."
-  pretty (TransformProof i o) = PP.vcat
-    [ PP.text "We transform the problem"
+  pretty (TransformFail err)      = PP.text $ "The transformation failed. The reason is " ++ err
+  pretty (TransformProof msg i o) = PP.vcat
+    [ (if null msg then PP.empty else PP.text msg) PP.<+> PP.text "The problem"
     , PP.indent 2 $ PP.pretty i
-    , PP.text "into the problem"
+    , PP.text "is transformed into the problem"
     , PP.indent 2 $ PP.pretty o ]
 
 instance (Xml.Xml i, Xml.Xml o) => Xml.Xml (TransformProof i o) where
-  toXml TransformFail        = Xml.elt "transformation" [ Xml.elt "failed" []]
-  toXml (TransformProof i o) =
+  toXml (TransformFail err)      = Xml.elt "transformation" [ Xml.elt "failed" [Xml.text err]]
+  toXml (TransformProof msg i o) =
     Xml.elt "transformation"
-      [ Xml.toXml i
+      [ Xml.text msg
+      , Xml.toXml i
       , Xml.toXml o ]
 
