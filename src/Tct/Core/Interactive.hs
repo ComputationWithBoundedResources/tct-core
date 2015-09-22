cd96e3b413385ec1be1d1338e8ccdd0dfda9796f
mschaper <michael.schaper@uibk.ac.at>
2015-09-22 13:23:08 +0200

    generalised interactive mode
    Now provides application of transformations (strategies with different
    input output type).
    
    Fixes computing progressed property. In the interactive mode, an application
    is successful when the result is continuing and one of the problems has
    progressed.
    
    Small API changes.

--- src/Tct/Core/Interactive.hs
+++ src/Tct/Core/Interactive.hs
@@ -1,20 +1,38 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
--- | This module provides a basic interactive functionality via ghci (Experimental and Unsafe).
+{-# LANGUAGE TypeOperators             #-}
+-- | This module provides a basic interactive functionality via \ghci\.
+--
+-- __WARNING:__ all operations that are directly applied to a problem are __unsafe__. More precisely, the application
+-- of a function to a problem is not type-safe: Given problems of type @i@. We can apply a strategy of type @Strategy
+-- o o@ (where @o@ different from @i@). This can lead to undefined behaviour.
 module Tct.Core.Interactive
   (
+  -- * Load and Modify Problems
   load
+  , modifyProblems
+  , onProblems
+  , onTree
+
+  -- * Select (Sub-)Problems
+  -- | We can select sub-problems individually and use 'applySelected' to apply strategies only on selected
+  -- sub-problems. Use 'printState' to  get a list of open sub-problems.
   , select
-  , unselect
+  , selectAll
 
-  , modifyProblem
-  , onProblem
+  -- * Apply Strategies
   , apply
+  , applySelected
 
-  , reset
+  -- * Undo History
   , undo
+  , reset
 
-  , state
-  , proof
+  -- * Print State
+  -- | For simplicity the output is fixed; And unrelated to user-defined options and configurations. Use 'onProblems',
+  -- 'onTree' to perform specific IO actions.
+  , printState
+  , printProof
+  , printDetailedProof
   ) where
 
 
@@ -50,14 +68,48 @@ selectLeafs ns pt = S.evalState (F.mapM k pt) 0
     k p = S.modify succ >> S.get >>= \i -> return (either (f i) (f i) p)
     f i = if i `elem` ns then Right else Left
 
-unselectLeafs :: ProofTree (Selected l) -> ProofTree (Selected l)
-unselectLeafs = fmap (either Right Right) where
+selectAllLeafs :: ProofTree (Selected l) -> ProofTree (Selected l)
+selectAllLeafs = fmap (either Right Right)
 
-evaluateSelected :: ProofData i => Strategy i i -> ProofTree (Selected i) -> TctM (Return (ProofTree (Selected i)))
-evaluateSelected _ pt@(Open (Left _))           = return (Continue pt)
-evaluateSelected s (Open (Right p))             = (fmap . fmap) Right `fmap` evaluate s p
-evaluateSelected s (NoProgress n subtree)       = liftNoProgress n `fmap` evaluateSelected s subtree
-evaluateSelected s (Progress n certfn subtrees) = liftProgress n certfn `fmap` (evaluateSelected s `F.mapM` subtrees)
+removeSelection :: ProofTree (Selected l) -> ProofTree l
+removeSelection = fmap (either id id)
+
+-- MS: The notion of progress in the interactive node is a bit different from the default semantics.
+-- Given for example @try poly@, we have progress when one of the selected leafs successfully applies the strategy.
+evaluateSelected :: ProofData i => Strategy i i -> ProofTree (Selected i) -> TctM (Bool, Return (ProofTree (Selected i)))
+evaluateSelected _ pt@(Open (Left _))           = return (False, Continue pt)
+evaluateSelected s (Open (Right p))             = do
+  ret <- evaluate s p
+  if isProgressing ret
+    then return (True,  (fmap . fmap) Right ret)
+    else return (False, (fmap . fmap) Right ret)
+evaluateSelected s (NoProgress n subtree)       = fmap (liftNoProgress n) `fmap` evaluateSelected s subtree
+evaluateSelected s (Progress n certfn subtrees) = do
+  f <- evaluateSelected s `F.mapM` subtrees
+  let
+    bs = fmap fst f
+    ns = fmap snd f
+  return (or (F.toList bs), liftProgress n certfn ns)
+
+-- MS: Strategies with (possible) different input output type have to be applied to all problems.
+evaluateAll :: ProofData o => Strategy i o -> ProofTree (Selected i) -> TctM (Bool, Return (ProofTree (Selected o)))
+evaluateAll s (Open (Left p))           = do
+  ret <- evaluate s p
+  if isProgressing ret
+    then return (True,  (fmap . fmap) Left ret)
+    else return (False, (fmap . fmap) Left ret)
+evaluateAll s (Open (Right p))             = do
+  ret <- evaluate s p
+  if isProgressing ret
+    then return (True,  (fmap . fmap) Right ret)
+    else return (False, (fmap . fmap) Right ret)
+evaluateAll s (NoProgress n subtree)       = fmap (liftNoProgress n) `fmap` evaluateAll s subtree
+evaluateAll s (Progress n certfn subtrees) = do
+  f <- evaluateAll s `F.mapM` subtrees
+  let
+    bs = fmap fst f
+    ns = fmap snd f
+  return (or (F.toList bs), liftProgress n certfn ns)
 
 
 --- * state ----------------------------------------------------------------------------------------------------------
@@ -68,91 +120,134 @@ data St l where
 unSt :: St l -> ProofTree (Selected l)
 unSt (St pt) = pt
 
-newtype State l = State { history_ :: [St l] }
+data a :+: b = a :+: b | Nil
 
-stateRef :: IORef (State l)
+null' :: (a :+: b) -> Bool
+null' Nil = True
+null' _   = False
+
+head' :: (a :+: b) -> a
+head' Nil       = error "Tct.Core.Interactive.head': empty list"
+head' (a :+: _) = a
+
+tail' :: (t :+: (a :+: b)) -> a :+: b
+tail' Nil       = Nil
+tail' (_ :+: b) = b
+
+newtype State l = State { history_ :: l }
+
+stateRef :: IORef (State (a :+: b))
 stateRef = unsafePerformIO $ newIORef st
-  where st = State { history_ = [] }
+  where st = State { history_ = Nil }
 {-# NOINLINE stateRef #-}
 
-getState :: IO (State l)
+getState :: IO (State (a :+: b))
 getState = readIORef stateRef
 
-putState :: State l -> IO ()
+putState :: State (a :+: b) -> IO ()
 putState = writeIORef stateRef
 
-modifyState :: (State l -> State l) -> IO ()
+modifyState :: (State (a :+: b) -> State (c :+: d)) -> IO ()
 modifyState f = getState >>= putState . f
 
-initSt :: ProofData l => l -> IO ()
-initSt st' = modifyState (\st -> st{ history_ = [St (Open (Right st'))] })
+initSt :: ProofData i => i -> IO ()
+initSt st' = modifyState (\st -> st{ history_ = St (Open (Right st')) :+: Nil })
 
-getSt :: IO (Maybe (St l))
+getSt :: IO (Maybe (St i))
 getSt = do
   hst <- history_ `fmap` getState
-  return $ if null hst then Nothing else Just (head hst)
+  return $ if null' hst then Nothing else Just (head' hst)
 
-maybeSt :: IO a -> (St l -> IO a) -> IO a
+maybeSt :: IO a -> (St i -> IO a) -> IO a
 maybeSt a f = getSt >>= maybe a f
 
-onSt :: (St l -> IO ()) -> IO ()
+onSt :: (St i -> IO ()) -> IO ()
 onSt = maybeSt (print "no problem specified")
 
-putSt :: St l -> IO ()
+putSt :: St i -> IO ()
 putSt st' = do
   st <- readIORef stateRef
-  writeIORef stateRef $ st { history_ = st': history_ st }
+  writeIORef stateRef $ st { history_ = st' :+: history_ st }
 
-modifySt :: ProofData l => (ProofTree (Selected l) -> ProofTree (Selected l)) -> IO ()
+modifySt :: ProofData o => (ProofTree (Selected i) -> ProofTree (Selected o)) -> IO ()
 modifySt f = onSt (putSt . St . f . unSt)
-
-printState :: IO ()
-printState = onSt (PP.putPretty . pp)
-  where pp (St l) = ppProofTreeLeafs PP.pretty l
 
 
 --- * interface ------------------------------------------------------------------------------------------------------
 
-load :: ProofData l => TctMode l l o -> FilePath -> IO ()
+-- | Load a problem.
+load :: ProofData i => TctMode i i o -> FilePath -> IO ()
 load m fp = do
   ret <- runErroneousIO $ tryIO (modeParser m fp) >>= liftEither . either (Left . TctParseError) Right
   either print (\prob -> initSt prob >> print "Problem loaded." >> printState) ret
 
-modifyProblem :: ProofData l => (l -> l) -> IO ()
-modifyProblem = modifySt . fmap . fmap
+-- | Modifies all sub-problems.
+modifyProblems :: ProofData i => (i -> i) -> IO ()
+modifyProblems = modifySt . fmap . fmap
 
-onProblem :: (l -> IO ()) -> IO ()
-onProblem f = onSt $ F.mapM_ f . rights . F.toList . unSt
+-- | Performs an IO action on selected sub-problems.
+onProblems :: (i -> IO()) -> IO ()
+onProblems f = onSt $ F.mapM_ f . rights . F.toList . unSt
 
+-- | Performs an IO action on the proof tree.
+onTree :: (ProofTree i -> IO()) -> IO ()
+onTree f = onSt $ f . removeSelection . unSt
+
+
+-- | Select a list of sub-problems.
 select :: [Int] -> IO ()
 select is = onSt $ \(St l) -> putSt (St (selectLeafs is l)) >> printState
 
-unselect :: IO ()
-unselect = onSt $ \(St l) -> putSt (St (unselectLeafs l)) >> printState
+-- | Select all sub-problems.
+selectAll :: IO ()
+selectAll = onSt $ \(St l) -> putSt (St (selectAllLeafs l)) >> printState
 
-apply :: ProofData i => Strategy i i -> IO ()
-apply str = onSt $ \st -> do
-  ret <- run defaultTctInteractiveConfig (evaluateSelected str $ unSt st)
-  if isProgressing ret
+
+apply' :: ProofData o => (Strategy i o -> ProofTree (Selected i) -> TctM (Bool, Return (ProofTree (Selected o)))) -> Strategy i o -> IO ()
+apply' eval str = onSt $ \st -> do
+  (b,ret) <- run defaultTctInteractiveConfig (eval str $ unSt st)
+  if b && isContinuing ret
     then putSt (St (fromReturn ret)) >> printState >> print "progressed :)"
     else print "no progress :/"
 
-proof :: IO ()
-proof = onSt (PP.putPretty . pp)
-  where pp (St l) = ppProofTree PP.pretty l
+-- | Applies a strategy on all sub-problems.
+apply :: ProofData o => Strategy i o -> IO ()
+apply = apply' evaluateAll
 
-state :: IO ()
-state = printState
+-- | Applies a strategy on \selected\ sub-problems. Requires the strategy to have the same input-output type.
+applySelected :: ProofData i => Strategy i i -> IO ()
+applySelected = apply' evaluateSelected
 
+
+-- | Undos state. Restores the prooftree before the last application of 'apply' and 'applySelected'.
 undo :: IO ()
 undo = do
   hst <- history_ `fmap` getState
-  unless (null hst) $ modifyState (\st -> st { history_ = tail hst })
+  unless (null' hst) $ modifyState (\st -> st { history_ = tail' hst })
   printState
 
+-- | Returns the the initial problem.
 reset :: IO ()
-reset = do
-  hst <- history_ `fmap` getState
-  unless (null hst) $ modifyState (\st -> st { history_ = [last hst] })
-  printState
+reset = reset' >> printState where
+  reset' = do
+    hst <- history_ `fmap` getState
+    case hst of
+      Nil           -> return ()
+      p@(_ :+: Nil) -> modifyState (\st -> st { history_ = p })
+      _             -> modifyState (\st -> st { history_ = tail' hst }) >> reset'
+
+-- | Print the proof.
+printProof :: IO ()
+printProof = onSt (PP.putPretty . pp)
+  where pp (St l) = ppProofTree PP.pretty l
+
+-- | Print detailed proof (including NoProgress nodes).
+printDetailedProof :: IO ()
+printDetailedProof = onSt (PP.putPretty . pp)
+  where pp (St l) = ppDetailedProofTree PP.pretty l
+
+-- | Print state, ie. a list of selected sub-problems.
+printState :: IO ()
+printState = onSt (PP.putPretty . pp)
+  where pp (St l) = ppProofTreeLeafs PP.pretty l
 
