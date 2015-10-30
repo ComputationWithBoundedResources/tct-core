4577dde96d3032a24ca4cb350ac5096baef3d714
mschaper <michael.schaper@uibk.ac.at>
2015-10-30 01:37:57 +0100

    fix spawning of commands
    We do it the hard/easy way.
    
    We use a patched version of `System.Process.readProcessWithExitCode` for
    proper spawning of processes and prevent the `waiForProcess: does not
    exist` errors.
    
    Seems to work fine now.
    
    This closes #6, and ComputationWithBoundedResources/tct-common#1.

--- src/Tct/Core/Common/Concurrent.hs
+++ src/Tct/Core/Common/Concurrent.hs
@@ -1,82 +1,104 @@
 module Tct.Core.Common.Concurrent
   ( spawn
-  , spawn'
   ) where
 
 
-import           Control.Concurrent      (forkIO)
-import           Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
-import qualified Control.Exception       as C
-import           Control.Monad           (void)
-import           System.Exit             (ExitCode (..))
+import           Control.Concurrent
+import           Control.Exception        (SomeException, mask, throwIO, try)
+import qualified Control.Exception        as C
+import           Control.Monad
+import           Foreign.C
+import           GHC.IO.Exception         (IOErrorType (..), IOException (..))
+import           System.Exit              (ExitCode (..))
 import           System.IO
-import           System.IO.Error         (tryIOError)
-import           System.Process
+import           System.Process           hiding (readCreateProcessWithExitCode)
+import           System.Process.Internals
 
 
 rnf :: String -> ()
 rnf = foldr seq ()
 
 -- | Replaces 'System.Process.readProcessWithExitCode' as this sometimes causes "waitForProcess: does not exist (No
--- child processes)" in our setting. Even when the computation is successfull.
+-- child processes)" in our setting. Even when the computation is successful.
 -- So we try to catch it here.
---
--- Returns Left error or Right ouput.
-spawn :: String -> [String] -> (Handle -> IO ()) -> IO (Either String String)
-spawn cmd args putInh = C.bracketOnError create release run where
-  create = do
-    (Just inh, Just outh, Just errh, ph)
-      <- createProcess (proc cmd args)
-        { std_in  = CreatePipe
-        , std_out = CreatePipe
-        , std_err = CreatePipe }
-    return (inh, outh, errh, ph)
+spawn :: String -> [String] -> IO (Either String String)
+spawn cmd args = do
+  (ex, out, err) <- readCreateProcessWithExitCode (proc cmd args) []
+  return $ case ex of
+    ExitFailure i -> Left ("Tct.Core.Common.Concurrent.spawn: Exitcode: " ++ show i ++ ": " ++ err)
+    ExitSuccess   -> Right out
 
-  release (inh, outh, errh, ph) = do
-    terminateProcess ph
-    _ <- tryIOError $ hClose inh
-    hClose outh >> hClose errh
-    _ <- forkIO $ C.catch (void $ waitForProcess ph) (\e -> void (return (e :: C.SomeException)))
-    return ()
+-- MS:
+-- Taken from System.Process 1.2.3.0
+-- 'System.Process.readCreateProcessWithExitCode' causes sometimes "waitForProcess: does not exist (No child process)"
+-- We patch it.
+readCreateProcessWithExitCode :: CreateProcess -> String -> IO (ExitCode,String,String)
+readCreateProcessWithExitCode cp input = do
+  let cp_opts = cp { std_in  = CreatePipe, std_out = CreatePipe, std_err = CreatePipe }
+  withCreateProcess_ "readCreateProcessWithExitCode" cp_opts $
+    \(Just inh) (Just outh) (Just errh) ph -> do
 
-  run (inh, outh, errh, ph) = do
-    outs <- hGetContents outh
-    errs <- hGetContents errh
-    outMVar <- newEmptyMVar
-    _ <- forkIO $ C.evaluate (rnf outs) >> putMVar outMVar ()
-    putInh inh
-    hClose inh
-    takeMVar outMVar
-    ex <- waitForProcess ph
-    return $ case ex of
-      ExitFailure i -> Left ("Tct.Core.Common.Concurrent.spawn: Exitcode: " ++ show i ++ ": " ++ errs)
-      ExitSuccess   -> Right outs
+      out <- hGetContents outh
+      err <- hGetContents errh
 
--- | Like 'spawn' but does not obtain any input.
-spawn' :: String -> [String] -> IO (Either String String)
-spawn' cmd args = C.bracketOnError create release run where
-  create = do
-    (_, Just outh, Just errh, ph)
-      <- createProcess (proc cmd args)
-        { std_out = CreatePipe
-        , std_err = CreatePipe }
-    return (outh, errh, ph)
+      -- fork off threads to start consuming stdout & stderr
+      withForkWait  (C.evaluate $ rnf out) $ \waitOut ->
+        withForkWait (C.evaluate $ rnf err) $ \waitErr -> do
 
-  release (outh, errh, ph) = do
-    terminateProcess ph
-    hClose outh
-    hClose errh
-    _ <- forkIO $ C.catch (void $ waitForProcess ph) (\e -> void (return (e :: C.SomeException)))
-    return ()
+        -- now write any input
+        unless (null input) $
+          ignoreSigPipe $ hPutStr inh input
+        -- hClose performs implicit hFlush, and thus may trigger a SIGPIPE
+        ignoreSigPipe $ hClose inh
 
-  run (outh, errh, ph) = do
-    outs <- hGetContents outh
-    errs <- hGetContents errh
-    outMVar <- newEmptyMVar
-    _ <- forkIO $ C.evaluate (rnf outs) >> putMVar outMVar ()
-    takeMVar outMVar
-    ex <- waitForProcess ph
-    return $ case ex of
-      ExitFailure i -> Left ("Tct.Core.Common.Concurrent.spawn: Exitcode: " ++ show i ++ ": " ++ errs)
-      ExitSuccess   -> Right outs
+        -- wait on the output
+        waitOut
+        waitErr
+
+        hClose outh
+        hClose errh
+
+      -- wait on the process
+      ex <- waitForProcess ph
+
+      return (ex, out, err)
+
+withForkWait :: IO () -> (IO () ->  IO a) -> IO a
+withForkWait async body = do
+  waitVar <- newEmptyMVar :: IO (MVar (Either SomeException ()))
+  mask $ \restore -> do
+    tid <- forkIO $ try (restore async) >>= putMVar waitVar
+    let wait = takeMVar waitVar >>= either throwIO return
+    restore (body wait) `C.onException` killThread tid
+
+ignoreSigPipe :: IO () -> IO ()
+ignoreSigPipe = C.handle $ \e ->
+  case e of
+    IOError { ioe_type  = ResourceVanished
+            , ioe_errno = Just ioe }
+      | Errno ioe == ePIPE -> return ()
+    _ -> throwIO e
+
+withCreateProcess_
+  :: String
+  -> CreateProcess
+  -> (Maybe Handle -> Maybe Handle -> Maybe Handle -> ProcessHandle -> IO a)
+  -> IO a
+withCreateProcess_ fun c action =
+  C.bracketOnError (createProcess_ fun c) cleanupProcess
+    (\(m_in, m_out, m_err, ph) -> action m_in m_out m_err ph)
+
+cleanupProcess :: (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle) -> IO ()
+cleanupProcess (mb_stdin, mb_stdout, mb_stderr, ph@(ProcessHandle _ delegating_ctlc)) = do
+  terminateProcess ph
+  maybe (return ()) (ignoreSigPipe . hClose) mb_stdin
+  maybe (return ()) hClose mb_stdout
+  maybe (return ()) hClose mb_stderr
+  when delegating_ctlc
+    stopDelegateControlC
+  -- _ <- forkIO (waitForProcess (resetCtlcDelegation ph) >> return ())
+  -- MS: here is actually the only change
+  _ <- forkIO $ void (waitForProcess (resetCtlcDelegation ph)) `C.catch` (\e -> void (return (e :: C.SomeException)))
+  return ()
+  where resetCtlcDelegation (ProcessHandle m _) = ProcessHandle m False
 
