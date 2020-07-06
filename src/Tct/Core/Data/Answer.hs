c99dc958b335cae54bf4e5985cfb42d86bf31719
Jonas Schöpf <jonas@schoepf.me>
2020-07-06 15:39:48 +0200

    prettify with brittany

--- src/Tct/Core/Data/Answer.hs
+++ src/Tct/Core/Data/Answer.hs
@@ -2,23 +2,24 @@
 module Tct.Core.Data.Answer
   (
   -- * Generic timebound format.
-  Timebounds (..)
+    Timebounds(..)
   , timebounds
   , prettyTermcompSep
   -- * tttac / termcomp (prior 2015) format
-  , TTTAC (..)
+  , TTTAC(..)
   , tttac
   -- * termcomp 2015 format
-  , Termcomp (..)
+  , Termcomp(..)
   , termcomp
-  ) where
+  )
+where
 
 
-import           Data.Monoid               ((<>))
-import qualified Tct.Core.Common.Pretty    as PP
-import qualified Tct.Core.Common.Xml       as Xml
+import           Data.Monoid                    ( (<>) )
+import qualified Tct.Core.Common.Pretty        as PP
+import qualified Tct.Core.Common.Xml           as Xml
 
-import qualified Tct.Core.Data.Certificate as T
+import qualified Tct.Core.Data.Certificate     as T
 
 
 -- | @Timebounds lower upper@
@@ -31,17 +32,18 @@ timebounds T.CertificateYesNo{} = Timebounds T.Unknown T.Unknown
 
 -- | Extracts lower and upper bounds of a certificate.
 timeboundsBestCase :: T.Certificate -> Timebounds
-timeboundsBestCase c@T.Certificate{}    = Timebounds (T.timeBCLB c) (T.timeBCUB c)
+timeboundsBestCase c@T.Certificate{} = Timebounds (T.timeBCLB c) (T.timeBCUB c)
 timeboundsBestCase T.CertificateYesNo{} = Timebounds T.Unknown T.Unknown
 
 
 instance PP.Pretty Timebounds where
-  pretty (Timebounds lb  ub) = PP.text "Timebounds" <> PP.tupled [PP.pretty lb, PP.pretty ub]
+  pretty (Timebounds lb ub) =
+    PP.text "Timebounds" <> PP.tupled [PP.pretty lb, PP.pretty ub]
 
 instance Xml.Xml Timebounds where
-  toXml (Timebounds lb ub) = Xml.elt "timebounds"
-    [ Xml.elt "lowerbound" [Xml.toXml lb]
-    , Xml.elt "upperbound" [Xml.toXml ub] ]
+  toXml (Timebounds lb ub) = Xml.elt
+    "timebounds"
+    [Xml.elt "lowerbound" [Xml.toXml lb], Xml.elt "upperbound" [Xml.toXml ub]]
 
 
 --- * termcomp -------------------------------------------------------------------------------------------------------
@@ -62,20 +64,20 @@ tttac c@T.Certificate{}      = TTTAC (timebounds c)
 tttac (T.CertificateYesNo x) = TTTACYN x
 
 instance PP.Pretty TTTAC where
-  pretty (TTTAC (Timebounds lb  ub))
-    | lb /= T.Unknown || ub /= T.Unknown = PP.text "YES" <> PP.tupled [PP.pretty lb, PP.pretty ub]
-  pretty (TTTACYN True) = PP.text "YES"
+  pretty (TTTAC (Timebounds lb ub)) | lb /= T.Unknown || ub /= T.Unknown =
+    PP.text "YES" <> PP.tupled [PP.pretty lb, PP.pretty ub]
+  pretty (TTTACYN True ) = PP.text "YES"
   pretty (TTTACYN False) = PP.text "NO"
-  pretty _ = PP.text "MAYBE"
+  pretty _               = PP.text "MAYBE"
 
 instance Xml.Xml TTTAC where
-  toXml (TTTAC (Timebounds lb ub))
-    | lb /= T.Unknown || ub /= T.Unknown = Xml.elt "certified"
-      [ Xml.elt "lowerbound" [Xml.toXml lb]
-      , Xml.elt "upperbound" [Xml.toXml ub] ]
-  toXml (TTTACYN True) = Xml.elt "YES" []
+  toXml (TTTAC (Timebounds lb ub)) | lb /= T.Unknown || ub /= T.Unknown =
+    Xml.elt
+      "certified"
+      [Xml.elt "lowerbound" [Xml.toXml lb], Xml.elt "upperbound" [Xml.toXml ub]]
+  toXml (TTTACYN True ) = Xml.elt "YES" []
   toXml (TTTACYN False) = Xml.elt "NO" []
-  toXml _  = Xml.elt "maybe" []
+  toXml _               = Xml.elt "maybe" []
 
 
 --- * termcomp -------------------------------------------------------------------------------------------------------
@@ -95,68 +97,78 @@ termcomp :: T.Certificate -> Termcomp
 termcomp c@T.Certificate{}      = Termcomp (timebounds c) (timeboundsBestCase c)
 termcomp (T.CertificateYesNo x) = TermcompYN x
 
-toTermcomp :: (t -> t -> t) -> t -> ((t1, t1) -> t) -> ((t1, t1) -> t) -> (Int -> t1) -> t1 -> (Int -> t1) -> t1 -> t1 -> Termcomp -> t
-toTermcomp sep maybeA worst best omegaA npolyA oA polyA unknownA (Termcomp (Timebounds lb ub) (Timebounds bclb bcub)) = output worst (lb, ub) `sep` output best (bclb, bcub)
-  where
-    output f (l, u) =
-      case (normlb l, normub u) of
-        (T.Unknown, T.Unknown) -> maybeA
-        (nlb, nub)             -> f (toclb nlb, tocub nub)
-    toclb T.Unknown         = unknownA
-    toclb (T.Poly (Just i)) = omegaA i
-    toclb _                 = npolyA
-    tocub T.Unknown         = unknownA
-    tocub (T.Poly (Just i)) = oA i
-    tocub _                 = polyA
-    normlb p@(T.Poly (Just i))
-      | i > 0 = p
-    normlb e@(T.Exp _) = e
-    normlb _ = T.Unknown
-    normub p@(T.Poly (Just i))
-      | i >= 0 = p
-    normub p@(T.Poly Nothing) = p
-    normub _ = T.Unknown
+toTermcomp
+  :: (t -> t -> t)
+  -> t
+  -> ((t1, t1) -> t)
+  -> ((t1, t1) -> t)
+  -> (Int -> t1)
+  -> t1
+  -> (Int -> t1)
+  -> t1
+  -> t1
+  -> Termcomp
+  -> t
+toTermcomp sep maybeA worst best omegaA npolyA oA polyA unknownA (Termcomp (Timebounds lb ub) (Timebounds bclb bcub))
+  = output worst (lb, ub) `sep` output best (bclb, bcub)
+ where
+  output f (l, u) = case (normlb l, normub u) of
+    (T.Unknown, T.Unknown) -> maybeA
+    (nlb      , nub      ) -> f (toclb nlb, tocub nub)
+  toclb T.Unknown         = unknownA
+  toclb (T.Poly (Just i)) = omegaA i
+  toclb _                 = npolyA
+  tocub T.Unknown         = unknownA
+  tocub (T.Poly (Just i)) = oA i
+  tocub _                 = polyA
+  normlb p@(T.Poly (Just i)) | i > 0 = p
+  normlb e@(T.Exp _)                 = e
+  normlb _                           = T.Unknown
+  normub p@(T.Poly (Just i)) | i >= 0 = p
+  normub p@(T.Poly Nothing)           = p
+  normub _                            = T.Unknown
 toTermcomp _ _ _ _ _ _ _ _ _ _ = error "not possible"
 
 
 instance PP.Pretty Termcomp where
-  pretty (TermcompYN True)  = PP.text "YES"
+  pretty (TermcompYN True ) = PP.text "YES"
   pretty (TermcompYN False) = PP.text "NO"
-  pretty c@Termcomp {}      = prettyTermcompSep (PP.<$$>) c
+  pretty c@Termcomp{}       = prettyTermcompSep (PP.<$$>) c
 
 
 prettyTermcompSep :: (PP.Doc -> PP.Doc -> PP.Doc) -> Termcomp -> PP.Doc
-prettyTermcompSep sep c =
-  toTermcomp
-    sep
-    (PP.text "MAYBE")
-    (\(lb, ub) -> PP.text "WORST_CASE" <> PP.tupled [lb, ub])
-    (\(lb, ub) -> PP.text "BEST_CASE" <> PP.tupled [lb, ub])
-    (\i -> PP.text "Omega" <> PP.parens (PP.text "n^" <> PP.int i))
-    (PP.text "NON_POLY")
-    (\i ->
-       PP.char 'O' <>
-       PP.parens
-         (if i == 0
-            then PP.int 1
-            else PP.text "n^" <> PP.int i))
-    (PP.text "POLY")
-    (PP.char '?')
-    c
+prettyTermcompSep sep c = toTermcomp
+  sep
+  (PP.text "MAYBE")
+  (\(lb, ub) -> PP.text "WORST_CASE" <> PP.tupled [lb, ub])
+  (\(lb, ub) -> PP.text "BEST_CASE" <> PP.tupled [lb, ub])
+  (\i -> PP.text "Omega" <> PP.parens (PP.text "n^" <> PP.int i))
+  (PP.text "NON_POLY")
+  (\i -> PP.char 'O'
+    <> PP.parens (if i == 0 then PP.int 1 else PP.text "n^" <> PP.int i)
+  )
+  (PP.text "POLY")
+  (PP.char '?')
+  c
 
 
 instance Xml.Xml Termcomp where
-  toXml (TermcompYN True)  = Xml.elt "YES" []
-  toXml (TermcompYN False) = Xml.elt "NO"  []
-  toXml c@Termcomp{} =
-    toTermcomp Xml.addChild
-      (Xml.elt "maybe" [])
-      (\(lb,ub) -> Xml.elt "worst_case" [Xml.elt "lowerbound" [lb], Xml.elt "upperbound" [ub]])
-      (\(lb,ub) -> Xml.elt "best_case" [Xml.elt "lowerbound" [lb], Xml.elt "upperbound" [ub]])
-      (\i -> Xml.elt "polynomial" [Xml.int i])
-      (Xml.text "non_poly")
-      (\i -> Xml.elt "polynomial" [Xml.int i])
-      (Xml.text "poly")
-      (Xml.text "unknown")
-      c
-
+  toXml (TermcompYN True ) = Xml.elt "YES" []
+  toXml (TermcompYN False) = Xml.elt "NO" []
+  toXml c@Termcomp{}       = toTermcomp
+    Xml.addChild
+    (Xml.elt "maybe" [])
+    (\(lb, ub) -> Xml.elt
+      "worst_case"
+      [Xml.elt "lowerbound" [lb], Xml.elt "upperbound" [ub]]
+    )
+    (\(lb, ub) -> Xml.elt
+      "best_case"
+      [Xml.elt "lowerbound" [lb], Xml.elt "upperbound" [ub]]
+    )
+    (\i -> Xml.elt "polynomial" [Xml.int i])
+    (Xml.text "non_poly")
+    (\i -> Xml.elt "polynomial" [Xml.int i])
+    (Xml.text "poly")
+    (Xml.text "unknown")
+    c
